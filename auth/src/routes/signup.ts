ebcaeb42bfb4c50625fa57a500e50efc9f552430
Shubham Tyagi <oyo@Shubhams-Mackbook-Pro.local>
2021-05-13 02:48:42 +0530

    mongoDB setup for user collection

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, response, Response} from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-errors';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post('/api/users/signup', [
       .isLength({min: 4, max: 20})
       .withMessage('Password must be between 4 and 20 characters')
   ], 
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     
     if (!errors.isEmpty()) {
@@ -22,11 +23,17 @@ router.post('/api/users/signup', [
     }
 
     const { email, password } = req.body;
+    
+    const existingUser = await User.findOne({ email });
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      throw new BadRequestError('Email already in use!');
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+    
+    res.status(201).send({user});
 });
 
 export { router as signupRouter };
