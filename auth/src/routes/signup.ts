b285aaa1f4319c572c758f51d5e37b5f83861053
Shubham Tyagi <oyo@Shubhams-Mackbook-Pro.local>
2021-05-26 02:52:15 +0530

    auth service implemented with singup signin signout currentUser

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
-import express, {Request, response, Response} from 'express';
-import { body, validationResult } from 'express-validator';
+import express, {Request, Response} from 'express';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-errors';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -14,24 +15,33 @@ router.post('/api/users/signup', [
       .trim()
       .isLength({min: 4, max: 20})
       .withMessage('Password must be between 4 and 20 characters')
-  ], 
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
 
     const { email, password } = req.body;
     
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email already in use!');
+      throw new BadRequestError('Email already in use!!');
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JSON web token 
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      }, process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
     
     res.status(201).send({user});
 });
